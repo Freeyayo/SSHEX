f058ba92efc65b8f7aff0536f313544dbbe10d81
Kevin_Cai <Kevin_Cai@epam.com>
2022-08-07 13:56:26 +0800

    using bean among beans

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -1,6 +1,7 @@
 package config;
 
 import main.Parrot;
+import main.Person;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,21 @@ public class ProjectConfig {
         var p = new Parrot();
         p.setName("Koko");
         return p;
+    }
+    @Bean
+    Person projectConfigManager () {
+        Person pcm = new Person();
+        pcm.setName("project configuration manager");
+        pcm.setPet(parrot());
+        return pcm;
+    }
+
+    @Bean
+    Person projectConfigManagerAssistant () {
+        Person pcma = new Person();
+        pcma.setName("project configuration manager assistant");
+        pcma.setPet(parrot());
+        return pcma;
     }
     @Bean
     String hello () {
