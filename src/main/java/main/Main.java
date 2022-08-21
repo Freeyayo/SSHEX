876ae1b84bae69ee9909f5d414449b4d9f79ce72
Kevin_Cai <Kevin_Cai@epam.com>
2022-08-21 21:03:38 +0800

    using comment service

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,26 @@
 package main;
 
+import model.Comment;
+import proxies.CommentNotificationProxy;
+import proxies.EmailCommentNotificationProxy;
+import repositories.CommentRepository;
+import repositories.DBCommentRepository;
+import services.CommentService;
+
 public class Main {
     public static void main(String[] args) {
+        CommentRepository commentRepository = new DBCommentRepository();
+        CommentNotificationProxy commentNotificationProxy = new EmailCommentNotificationProxy();
 
+        CommentService commentService = new CommentService(
+                commentRepository,
+                commentNotificationProxy
+        );
+
+        Comment comment = new Comment();
+        comment.setAuthor("Kevin");
+        comment.setText("Four thousand weeks must be good to read");
+
+        commentService.publishComment(comment);
     }
 }
